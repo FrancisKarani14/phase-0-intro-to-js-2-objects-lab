fd1f437e5a8fded4bf5727a031e084a21f6e7bbe
Francis <franciskarani15@gmail.com>
2025-06-11 04:33:58 +0300

    adds and updates the empletees objects

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+  name: "Alice Smith",
+  streetAddress: "123 Main St"
+};
+
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+ 
+  return { ...employeeObj, [key]: value };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+  
+  employeeObj[key] = value;
+  return employeeObj; 
+}
+
+
+function deleteFromEmployeeByKey(employeeObj, key) {
+  
+  const { [key]: _, ...newEmployee } = employeeObj;
+  return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+ 
+  delete employeeObj[key];
+  return employeeObj; 
+}
+
+
+
+module.exports = {
+  employee, 
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey
+};
